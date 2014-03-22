ccce50995d37fd2f09c74227feaeda6ee3aeb310
iLeW <glexmad@gmail.com>
2014-03-22 12:51:31 +0100

    Initial checkin

---
+++ DoyProject/src/demo/Main.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		displayGreeting();
+	}
+	
+	//Metodo privato, statico (usabile senza creare una istanza della classe, e void (che non torna nulla) per stampare saluti
+	private static void displayGreeting(){
+		System.out.println("Hello World!");
+		System.out.println("Prova di stampa.");
+		
+		
+	}
+
+}
